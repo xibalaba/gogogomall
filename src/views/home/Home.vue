e7462c1b8f6e9fd986c06a92b98818a719bd381d
“xibalaba” <100498459+xibalaba@users.noreply.github.com>
2022-03-04 15:59:02 +0800

    初始化项目

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,137 @@
 <template>
-  <h2>首页</h2>
+  <div id="home" class="wrapper">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+    <home-swiper :banners="banners" />
+    <recommend-view :recommends="recommends" />
+    <feature-view />
+    <tab-control class="tab-control" :titles="['流行','新款','精选']" @tabClick="tabClick" />
+    <good-list :goods="showGoods" />
+  </div>
 </template>
 
 <script>
-  export default {
-    name: "Home"
+import HomeSwiper from './childComps/HomeSwiper'
+import RecommendView from './childComps/RecommendView'
+import FeatureView from './childComps/FeatureView'
+
+import NavBar from 'components/common/navbar/NavBar'
+import TabControl from 'components/content/tabControl/TabControl'
+import GoodList from 'components/content/goods/GoodsList'
+
+import { getHomeMultidata, getHomeGoods } from "network/home"
+
+
+export default {
+  name: "Home",
+  components: {
+    HomeSwiper,
+    RecommendView,
+    FeatureView,
+    NavBar,
+    TabControl,
+    GoodList
+  },
+  data () {
+    return {
+      banners: [],
+      recommends: [],
+      goods: {
+        'pop': { page: 0, list: [] },
+        'new': { page: 0, list: [] },
+        'sell': { page: 0, list: [] },
+      },
+      currentType: 'pop',
+      isShowBackTop: false
+    }
+  },
+  computed: {
+    showGoods () {
+      return this.goods[this.currentType].list
+    }
+  },
+  created () {
+    // 1.请求多个数据
+    this.getHomeMultidata()
+    // 2.请求商品数据
+    this.getHomeGoods('pop')
+    this.getHomeGoods('new')
+    this.getHomeGoods('sell')
+  },
+  methods: {
+    tabClick (index) {
+      switch (index) {
+        case 0:
+          this.currentType = 'pop'
+          break
+        case 1:
+          this.currentType = 'new'
+          break
+        case 2:
+          this.currentType = 'sell'
+          break
+      }
+    },
+    backClick () {
+      this.$refs.scroll.scrollTo(0, 0)
+    },
+    contentScroll (position) {
+      this.isShowBackTop = (-position.y) > 1000
+    },
+    loadMore () {
+      this.getHomeGoods(this.currentType)
+    },
+    getHomeMultidata () {
+      getHomeMultidata().then(res => {
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      })
+    },
+    getHomeGoods (type) {
+      const page = this.goods[type].page + 1
+      getHomeGoods(type, page).then(res => {
+        this.goods[type].list.push(...res.data.list)
+        this.goods[type].page += 1
+
+        // this.$refs.scroll.finishPullUp()
+      })
+    }
   }
+}
 </script>
 
 <style scoped>
+#home {
+  /*padding-top: 44px;*/
+  height: 100vh;
+  position: relative;
+}
 
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 9;
+}
+
+.tab-control {
+  position: sticky;
+  top: 44px;
+  z-index: 9;
+}
+
+.content {
+  overflow: hidden;
+
+  position: absolute;
+  top: 44px;
+  bottom: 49px;
+  left: 0;
+  right: 0;
+}
 </style>
