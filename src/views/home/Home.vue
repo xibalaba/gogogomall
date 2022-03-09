758bca2d6da0cee31d104a7aa4a3805a1b66cf7d
“xibalaba” <100498459+xibalaba@users.noreply.github.com>
2022-03-09 16:27:09 +0800

    3-9

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,33 @@
 <template>
-  <div id="home" class="wrapper">
-    <nav-bar class="home-nav">
+  <div id="home">
+    <nav-bar class="nav-bar">
       <div slot="center">购物街</div>
     </nav-bar>
-    <home-swiper :banners="banners" />
-    <recommend-view :recommends="recommends" />
-    <feature-view />
-    <tab-control class="tab-control" :titles="['流行','新款','精选']" @tabClick="tabClick" />
-    <good-list :goods="showGoods" />
+    <tab-control
+      v-show="isTabFixed"
+      class="fixed"
+      @itemClick="tabClick"
+      :titles="['流行', '新款', '精选']"
+    ></tab-control>
+    <scroll
+      class="content"
+      ref="scroll"
+      :probe-type="3"
+      @scroll="contentScroll"
+      :pull-up-load="true"
+      @pullingUp="loadMore"
+    >
+      <div>
+        <home-swiper :banners="banners" />
+        <recommend-view :recommends="recommends" />
+        <feature-view />
+        <tab-control ref="tabControl" :titles="['流行','新款','精选']" @tabClick="tabClick" />
+        <good-list :goods="showGoods" />
+      </div>
+    </scroll>
+    <back-top @backTop="backTop" class="back-top" v-show="showBackTop">
+      <img src="~assets/img/common/top.png" alt />
+    </back-top>
   </div>
 </template>
 
@@ -19,6 +39,8 @@ import FeatureView from './childComps/FeatureView'
 import NavBar from 'components/common/navbar/NavBar'
 import TabControl from 'components/content/tabControl/TabControl'
 import GoodList from 'components/content/goods/GoodsList'
+import Scroll from 'components/common/scroll/Scroll'
+import BackTop from 'components/content/backTop/BackTop'
 
 import { getHomeMultidata, getHomeGoods } from "network/home"
 
@@ -31,7 +53,9 @@ export default {
     FeatureView,
     NavBar,
     TabControl,
-    GoodList
+    GoodList,
+    Scroll,
+    BackTop
   },
   data () {
     return {
@@ -43,7 +67,10 @@ export default {
         'sell': { page: 0, list: [] },
       },
       currentType: 'pop',
-      isShowBackTop: false
+      isShowBackTop: false,
+      isTabFixed: false,
+      tabOffsetTop: 0,
+      showBackTop: true
     }
   },
   computed: {
@@ -73,11 +100,16 @@ export default {
           break
       }
     },
-    backClick () {
-      this.$refs.scroll.scrollTo(0, 0)
+    backTop () {
+
+      this.$refs.scroll.scrollTo(0, 0, 300)
     },
     contentScroll (position) {
-      this.isShowBackTop = (-position.y) > 1000
+      // 1.决定tabFixed是否显示
+      this.isTabFixed = position.y < -this.tabOffsetTop
+
+      // 2.决定backTop是否显示
+      this.showBackTop = position.y < -1000
     },
     loadMore () {
       this.getHomeGoods(this.currentType)
@@ -94,7 +126,7 @@ export default {
         this.goods[type].list.push(...res.data.list)
         this.goods[type].page += 1
 
-        // this.$refs.scroll.finishPullUp()
+        this.$refs.scroll.finishPullUp()
       })
     }
   }
@@ -103,35 +135,35 @@ export default {
 
 <style scoped>
 #home {
-  /*padding-top: 44px;*/
+  /*position: relative;*/
   height: 100vh;
-  position: relative;
 }
 
-.home-nav {
+.nav-bar {
   background-color: var(--color-tint);
+  font-weight: 700;
   color: #fff;
-
-  position: fixed;
-  left: 0;
-  right: 0;
-  top: 0;
-  z-index: 9;
-}
-
-.tab-control {
-  position: sticky;
-  top: 44px;
-  z-index: 9;
 }
 
 .content {
-  overflow: hidden;
-
   position: absolute;
   top: 44px;
   bottom: 49px;
   left: 0;
   right: 0;
+  background-color: #fff;
+}
+
+.fixed {
+  position: fixed;
+  top: 44px;
+  left: 0;
+  right: 0;
+}
+
+.back-top {
+  position: fixed;
+  right: 10px;
+  bottom: 60px;
 }
 </style>
